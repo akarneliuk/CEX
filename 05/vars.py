27c587bcb572cc96384481dc97acda22833c53d2
akarneliuk <akarneliuk@gmail.com>
2020-03-01 17:29:59 +0000

    05

--- 05/vars.py
+++ 05/vars.py
@@ -1,21 +1,19 @@
 #!/usr/local/bin/python3.8
 
-initial_string = 'pe1-fra-de,Equinix FR5,10.11.12.13/24,,Cisco NCS 5500'
+input_string = 'PE1-BLN-DE,Equinix FR5,10.0.0.11/24,,Cisco NCS 5500'
 
-dev_params = initial_string.split(',')
+dev_params = input_string.split(',')
 
-print('The variables is split into: {}'.format(dev_params))
+print('The parsed values are {}'.format(dev_params))
 
 ip_addr, ip_prefix = dev_params[2].split('/')
 
-print('The IP address is {} with the prefix {}'.format(ip_addr, ip_prefix))
+print('The IP is {} with the prefix {}'.format(ip_addr, ip_prefix))
 
 final_string = ','.join(dev_params)
 
-print('The string joined back: {}'.format(final_string))
-
+print('The final string is {}'.format(final_string))
 
 ip_addr_pref = '/'.join([ip_addr, ip_prefix])
 
-print('The IP address and prefix together is {}'.format(ip_addr_pref))
-
+print('The IP and prefix is {}'.format(ip_addr_pref))

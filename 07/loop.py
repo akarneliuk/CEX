94123478ba82b4820f8e4c452b749db4f8ba1b7c
akarneliuk <akarneliuk@gmail.com>
2020-03-15 18:27:22 +0000

    07

--- 07/loop.py
+++ 07/loop.py
@@ -1,30 +1,25 @@
 #!/usr/local/bin/python3.8
 
-interfaces = [{'name': 'Ethernet1'}, {'name': 'Ethernet2'}, {'name': 'Ethernet3'}]
+interfaces = [{'name': 'Ethernet1'}, {'name': 'Ethernet2'}]
 
-print('The list of the interfaces:')
-
-for interface in interfaces:
-    print('Your interfaces is {}'.format(interface))
-    print('More precisely {}'.format(interface['name']))
+for interface_entry in interfaces:
+    print('Your element is {}'.format(interface_entry))
+    print('The key name from element is {}'.format(interface_entry['name']))
 
 
 interfaces2 = {
                 'Ethernet1': {
-                               'speed': 10000,
-                               'enabled': True
-                             },
+                  'speed': 10000,
+                  'enabled': True
+                },
                 'Ethernet2': {
-                               'speed': 10000,
-                               'enabled': True
-                             },
-                'Ethernet3': {
-                               'speed': 10000,
-                               'enabled': True
-                             }
+                  'speed': 10000,
+                  'enabled': True
+                }
               }
 
-print('The dictionary of the interfaces:')
+print('original dict {}'.format(interfaces2))
+print('transformed dict {}'.format(interfaces2.items()))
 
-for interface_key, interface_value in interfaces2.items():
-    print('Internface {} has {} speed and it is {}ly enabled'.format(interface_key, interface_value['speed'], interface_value['enabled']))
+for int_key, int_value in interfaces2.items():
+    print('The key is {} and the value is {}'.format(int_key, int_value))

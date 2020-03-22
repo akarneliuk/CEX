372adf45b9cea3e60fa14c87c1c3a55a6294a317
akarneliuk <akarneliuk@gmail.com>
2020-03-22 14:47:40 +0000

    0.8

--- 08/conditions.py
+++ 08/conditions.py
@@ -1,9 +1,9 @@
 #!/usr/local/bin/python3.8
 
 interfaces = [
-               {'name': 'Ethernet1', 'enabled': True, 'ipv4': '192.168.100.1/24'},
-               {'name': 'Ethernet2', 'enabled': False, 'vlan': 'tagged'},
-               {'name': 'Ethernet3', 'enabled': True, 'ipv4': '192.168.101.1/24', 'vlan': 'tagged'}
+               {'name': 'Eth1', 'enabled': True, 'ipv4': '10.0.0.1/24'},
+               {'name': 'Eth2', 'enabled': False, 'vlan': 'tagged'},
+               {'name': 'Eth3', 'enabled': True, 'ipv4': '10.0.1.1/24', 'vlan': 'untagged'}
              ]
 
 config_lines = []
@@ -21,24 +21,24 @@ for iface_entry in interfaces:
         config_lines.append('  no switchport')
 
         if 'ipv4' in iface_entry:
-           config_lines.append('  ipv4 address {}'.format(iface_entry['ipv4']))
+            config_lines.append('  ipv4 address {}'.format(iface_entry['ipv4']))
 
         if 'ipv6' in iface_entry:
-           config_lines.append('  ipv6 address {}'.format(iface_entry['ipv6']))
+            config_lines.append('  ipv6 address {}'.format(iface_entry['ipv6']))
 
     elif 'vlan' in iface_entry and not ('ipv4' in iface_entry or 'ipv6' in iface_entry):
         config_lines.append('  switchport')
 
         if iface_entry['vlan'] == 'tagged':
-           config_lines.append('  switchport mode trunk')
+            config_lines.append('  switchport mode trunk') 
 
-        elif iface_entry['vlan'] == 'untagged':
-           config_lines.append('  switchport mode access')    
+        elif iface_entry['vlan'] == 'access':
+            config_lines.append('  switchport mode access') 
 
     else:
-        config_lines.pop(-1)
-        config_lines.append('  descriontion THERE IS SOME MESS WITH VARS')
-        config_lines.append('  shutdown')
+       config_lines.pop(-1)
+       config_lines.append('  shutdown')
+       config_lines.append('  description "THERE IS A MESS WITH VARS"')
 
     config_lines.append('!')
 

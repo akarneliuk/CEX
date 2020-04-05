099d507b76f15380523d9a17a85c954533d1dfbf
akarneliuk <akarneliuk@gmail.com>
2020-04-05 14:14:52 +0100

    10

--- 10/def.py
+++ 10/def.py
@@ -3,38 +3,37 @@
 # Variables
 ip_addresses = '10.0.0.3-10.0.1.15'
 
-
-# User-defined functions
-def ip_int_str(operation, input_var):
+# User-defined function
+def ip_str_int(input_var, operation):
     if operation == 'get_int':
-        list_addr = input_var.split('.')
-        temp_number = 0
-        index = len(list_addr) - 1
+        input_var = input_var.split('.')
+        index = len(input_var) - 1
+        temp_value = 0
 
-        while index >=0:
-            temp_number += (int(list_addr[index]) * (256 ** (len(list_addr) - 1 - index)))
+        while index >= 0:
+            temp_value += (int(input_var[index]) * (256 ** (len(input_var) - 1 - index)))
             index -= 1
 
     elif operation == 'get_str':
         decr = input_var
         index = 0
-        temp_number = []
+        temp_value = []
 
         while decr > 0:
-            temp_number.append(str(int(decr / 256 ** (3 - index))))
+            temp_value.append(str(int(decr / 256 ** (3 - index))))
             decr %= (256 ** (3 - index))
             index += 1
 
-        temp_number = '.'.join(temp_number)
+        temp_value = '.'.join(temp_value)
 
-    return temp_number
+    return temp_value
 
 
-# Main body
+# Body
 ip_start, ip_end = ip_addresses.split('-')
 
-num_ip_s = ip_int_str('get_int', ip_start)
-num_ip_e = ip_int_str('get_int', ip_end)
+num_ip_s = ip_str_int(ip_start, 'get_int')
+num_ip_e = ip_str_int(ip_end, 'get_int')
 
 if num_ip_s <= num_ip_e:
     num_ip_t = num_ip_s
@@ -42,8 +41,9 @@ if num_ip_s <= num_ip_e:
     print('Printing IP in range {}'.format(ip_addresses))
 
     while num_ip_t <= num_ip_e:
-        ip_addr = ip_int_str('get_str', num_ip_t)
-        print('IP is {}'.format(ip_addr))
+        ip_addr_str = ip_str_int(num_ip_t, 'get_str')
+
+        print('IP is {}'.format(ip_addr_str))
 
         num_ip_t += 1
 
